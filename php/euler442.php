8501740974944047d92341544238b021c59dae46
Joe Colburn <joecolburn@quickenloans.com>
2013-10-31 23:48:05 -0400

    almost there

--- php/euler442.php
+++ php/euler442.php
@@ -11,65 +11,156 @@
  * Solution: 
  */
 
+// 11
+// 121
+// 1331
+// 14641
+// 161051
+// 1771561
+// 19487171
+// 214358881
+// 2357947691
+// 25937424601
+// 285311670611
+// 3138428376721
+// 34522712143931
+// 379749833583241
+// 4177248169415651
+// 45949729863572161
+// 505447028499293771
+// 5559917313492231481
+
+// ALL
+// 200 = 13
+// every addl hundred is 1 more
+// 1000 = 149
+// every addl 1000 is 20 more
+// 10000 is 290
+// 20000 is 1659
+// 30000 is 1948
+// 40000 is 2237
+// 50000 is 2526
+
+// Just 11
+// 100   is 1
+// 200   is 11
+// 300   is 12
+// 400   is 13
+// 500   is 14
+// 600   is 15
+// 700   is 16
+// 800   is 17
+// 900   is 18
+// 1000  is 19  (109 delta or 100 plus 9*1)
+// 2000  is 128 (19 delta or the number from 1000)
+// 3000  is 147 (19 delta)
+// 4000  is 166 (19 delta)
+// 5000  is 185 (19 delta)
+// 6000  is 204 (19 delta)
+// 7000  is 223 (19 delta)
+// 8000  is 242 (19 delta)
+// 9000  is 261 (19 delta)
+// 10000 is 280 (19 delta)
+// 20000 is 1451 (1171 delta or 1000 plus 9*19)
+// 30000 is 1731 (280 delta or the number from 10000)
+// 40000 is 2011 (280 delta)
+// 50000 is 2291 (280 delta)
+// 60000 is 2571 (280 delta)
+// 70000 is 2851 (280 delta)
+// 80000 is 3131 (280 delta)
+// 90000 is 3411 (280 delta)
+// 100000 is 3691 (280 delta)
+
 include "helper.php";
 
 function powers()
 {
     global $powers_of_e, $last;
 
-    for ($i=$last+1; $i < $last + 20; $i++) {
-        $powers_of_e[] = pow(11, $i);
+    for ($i=$last+1; $i < $last + 18; $i++) {
+        $powers_of_e[] = (string) pow(11, $i);
     }
 }
 
-function eFree()
-{
-    global $powers_of_e, $num;
 
-    if ($num > $powers_of_e[count($powers_of_e)-1]) {
-        powers();
-    }
-    foreach ($powers_of_e as $idx => $pow) {
-        if ($pow > $num) {
-            return true;
-        }
-        if (strstr((string) $num, (string) $pow)) {
-            // $cnt = strlen($num) - 1;
-            // Skip forward to avoid unneccessary checks
-            // commented out for now as it produces incorrect results with very little speed gain.
-            // if (strstr(substr((string) $num, 0, $cnt), (string) $pow)) {
-            //     $new = (string) $pow + 1;
-            //     $num = str_replace((string) $pow, $new, (string) $num);
-            // }
-
-            return false;
-        }
-    }
-
-    return true;
-}
-
-function e($pos) {
-    global $num;
-
-    $x = 0;
-    while ($x < $pos) {
-        $num++;
-        if (eFree()) {
-            $x++;
-        }
-    }
-
-    return $num;
-}
-
-$powers_of_e = array(11);
+$powers_of_e = array("121");
 $last = 1;
-$num = 0;
+$max = 200;
+// $max = pow(10, 18);
 
-// var_dump(e(pow(10, 6))); // 5 =  1.8 sec ... 6 = 23 sec ... 7 = 288 sec
-var_dump(e(500000)); // 10 seconds @ 500,000
-// var_dump(e(pow(10, 18)));
+// powers();
 
+// Real stuff
+
+$max = 6;
+
+$count = $prev = 1;
+for ($i=1; $i < $max; $i++) {
+    if ($i > 2) {
+        $addl = $prev * 9;
+    } else {
+        $addl = 0;
+    }
+    $count = $count * 9 + pow(10, $i) + $addl;
+    echo "for " . pow(10, $i+2) . " count is $count\n";
+}
+var_dump($count);
+
+die();
+
+
+
+
+
+// tester
+$testers = array(
+    100,
+    200,
+    300,
+    400,
+    500,
+    600,
+    700,
+    800,
+    900,
+    1000,
+    2000,
+    3000,
+    4000,
+    5000,
+    6000,
+    7000,
+    8000,
+    9000,
+    10000,
+    20000,
+    30000,
+    40000,
+    50000,
+    60000,
+    70000,
+    80000,
+    90000,
+    100000,
+    200000,
+    300000,
+    400000,
+    500000,
+    600000,
+    700000,
+    800000,
+    900000,
+    1000000
+    );
+foreach ($testers as $max) {
+    $count = 0;
+    for ($i=1; $i < $max; $i++) {
+        if (strstr((string) $i, (string) $powers_of_e[0])) {
+            $count++;
+        }
+    }
+    echo "$max has $count non-free in it.\n";
+}
 
 result(1, 1);
+
