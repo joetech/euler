2fd1e951f23c849defc2d37ddf89be04f7f16d60
Joe Colburn <joecolburn@quickenloans.com>
2013-11-18 08:44:23 -0500

    still not done

--- php/euler439.php
+++ php/euler439.php
@@ -10,27 +10,6 @@
 
 include "helper.php";
 
-// function d($k)
-// {
-// 	global $factors_of;
-
-// 	$factors = array();
-
-//     for ($i=$k; $i >= 1; $i--) {
-// 		// if (array_key_exists($i, $factors_of)) {
-// 		// 	$factors = array_merge($factors, $factors_of[$i]);
-// 		// } else {
-// 	        if ($k % $i == 0) {
-// 	            $factors[] = $i;
-// 	        }
-// 		// }
-//     }
-
-//     $factors_of[$k] = array_unique($factors);
-
-//     return $factors_of[$k];
-// }
-
 function d($k)
 {
 	$factors = array(1,$k);
@@ -60,7 +39,7 @@ function s($n)
 
 	$sum = 0;
 
-	for ($i=1; $i <= $n; $i++) { 
+	for ($i=1; $i <= $n; $i++) {
 		for ($j=1; $j <= $n; $j++) {
 			$num = $i * $j;
 			if (isset($sums[$num])) {
@@ -72,7 +51,7 @@ function s($n)
 				$sums[$num] = array_sum($factors_of[$num]);
 				$sum += $sums[$num];
 			}
-			// echo "sum is $sum\n";
+			// echo "sum $i and $j is $sum\n";
 		}
 	}
 
@@ -81,36 +60,39 @@ function s($n)
 
 $factors_of = $sums = array();
 
-// $tried = $hit = 0;
-// for ($i=999; $i <= 400000; $i+=1000) {
-// 	$d = d($i);
-// 	if (substr($d, -3) == '864') {
-// 		echo $i . " is " . $d . "\n";
-// 		$hit++;
-// 	}
-// 	$tried++;
+
+
+// echo s(pow(10,2));
+// echo s(1) . "\n";;
+// echo s(2) . "\n";;
+// echo s(3) . "\n";;
+// echo s(4) . "\n";;
+// echo s(5) . "\n";;
+// echo s(6) . "\n";;
+// echo s(7) . "\n";;
+// echo s(8) . "\n";;
+// echo s(9) . "\n";;
+
+
+// echo "s(800) is " . s(800) . "\n";
+
+$divisor = 144;
+for ($i=1; $i <= 300; $i++) {
+    $sum = s($i);
+    $next = $sum / $divisor;
+    if ($sum % $divisor == 0) {
+        echo "s(".$i.") is $sum ( / is $next)\n";
+    }
+}
+
+// for ($i=1; $i <= 50; $i++) {
+//     $d = d($i);
+//     sort($d);
+//     $txt = implode('.', $d);
+//     echo "============= $i : $txt\n";
 // }
-// echo "$hit of $tried matched pattern.\n";
 
-// echo "-------------------------\n";
-
-// for ($i=1; $i <= 6; $i+=1) {
-// 	$s = s(pow(10,$i));
-// 	// if (substr($s, -3) == '000') {
-// 		echo "s(10^" . $i . ") is " . $s . "\n";
-// 	// }
-// }
-
-// for ($i=99; $i <= 200; $i+=10) {
-// 	$s = s($i);
-// 	// if (substr($s, -3) == '000') {
-// 		echo $i . " is " . $s . "\n";
-// 	// }
-// }
-
-echo s(pow(10,11)) % pow(10,9) . "\n";
-// echo s(pow(10, 3)) . "\n";
-// echo (pow(10, 9) * 215766508) % 563576517282 . "\n";
+//================================================================
 
 
 result(1,1);
